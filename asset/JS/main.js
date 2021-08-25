b34b588be10719fcf91c84abaa938cd42f145ea8
Dam Luu Trung Hieu <hieuddeu@gmail.com>
2021-08-25 11:09:40 +0700

    update

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -26,7 +26,59 @@ for (let item of navListItems) {
         }
     }
 }
-// End Header
+//  End header
+
+
+
+// SCROLL TOP
+
+const scrollTop = $('.scroll-top')
+
+function scrollBackTop() {
+    const scrollY = window.pageYOffset
+    if (scrollY > 500) {
+        scrollTop.classList.add('scroll-active')
+    } else {
+        scrollTop.classList.remove('scroll-active')
+    }
+}
+// Scroll Active link
+const sections = $$('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop
+        sectionId = current.getAttribute('id')
+        if (scrollY > sectionTop && scrollY <= sectionHeight + sectionTop) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+}
+
+// SCROLL Change background header
+const header = $('#header')
+
+function activeBgHeader() {
+    const scrollY = window.pageYOffset
+    if (scrollY > 50) {
+        header.classList.add('active-bg')
+    } else {
+        header.classList.remove('active-bg')
+    }
+}
+
+//  SCROLL ACTION
+window.onscroll = function() {
+    activeBgHeader()
+    scrollActive()
+    scrollBackTop()
+}
+
 
 // Skill
 let skillHeader = $('.skill__header')
@@ -84,3 +136,25 @@ modalCloses.forEach((btnClose, i) => {
         closeModal(i)
     }
 })
+
+
+// =========== DARK LIGHT THEME ==================
+
+const themeBtn = $('#theme-btn')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeBtn.classList.contains(iconTheme) ? 'uil-sun' : 'uil-moon'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeBtn.classList[selectedIcon === 'uli-moon' ? 'add' : 'remove'](iconTheme)
+}
+themeBtn.onclick = function() {
+    document.body.classList.toggle(darkTheme)
+    themeBtn.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+}
